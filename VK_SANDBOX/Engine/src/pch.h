a8eb9b45f497951c46389e7ebcb8039681e19a38
Mark Chen (I Hsuan Chen) <markchen0819@gmail.com>
2023-05-05 15:50:36 -0700

    using pch

---
+++ VK_SANDBOX/Engine/src/pch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// C++
+#include <iostream>
+
+
+// GLFW & VULKAN
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+
