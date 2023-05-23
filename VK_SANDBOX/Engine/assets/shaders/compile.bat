f8fbc3e81cd64e9d95ecd3ebe47773f6e0047a1e
Mark Chen (I Hsuan Chen) <markchen0819@gmail.com>
2023-05-22 18:27:08 -0700

    First triangle
    https://vulkan-tutorial.com/Drawing_a_triangle/Drawing/Rendering_and_presentation

---
+++ VK_SANDBOX/Engine/assets/shaders/compile.bat
@@ -0,0 +1,5 @@
+%~dp0\..\..\..\Libraries\VulkanSDK\1.3.243.0\Bin\glslc.exe shader.vert -o vert.spv
+%~dp0\..\..\..\Libraries\VulkanSDK\1.3.243.0\Bin\glslc.exe shader.frag -o frag.spv
+pause
+
+
--- VK_SANDBOX/Engine/src/pch.h
+++ VK_SANDBOX/Engine/src/pch.h
@@ -2,15 +2,25 @@
 
 // C++
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <optional>
+#include <algorithm>
 
 
 // GLFW & VULKAN
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+//#define GLM_FORCE_RADIANS
+//#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+//#include <glm/vec4.hpp>
+//#include <glm/mat4x4.hpp>
+
+
 
 
