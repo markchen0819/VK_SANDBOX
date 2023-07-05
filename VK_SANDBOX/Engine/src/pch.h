dbea033bf3bc492f5777823e3fd36cfae2b39bf9
Mark Chen (I Hsuan Chen) <markchen0819@gmail.com>
2023-07-04 18:15:52 -0700

    Most vk tutorial done
    https://vulkan-tutorial.com/

--- VK_SANDBOX/Engine/src/pch.h
+++ VK_SANDBOX/Engine/src/pch.h
@@ -5,22 +5,23 @@
 #include <fstream>
 #include <stdexcept>
 #include <cstdlib>
+#include <algorithm>
+#include <optional>
 #include <cstring>
 #include <vector>
+#include <array>
 #include <set>
-#include <optional>
-#include <algorithm>
-
+#include <unordered_map>
 
 // GLFW & VULKAN
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-//#define GLM_FORCE_RADIANS
-//#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-//#include <glm/vec4.hpp>
-//#include <glm/mat4x4.hpp>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
-
-
-
+#include <chrono>
