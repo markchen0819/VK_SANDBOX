c92bd1307b15cad25b0ddc325a82750939eb3234
Mark Chen (I Hsuan Chen) <markchen0819@gmail.com>
2023-09-25 03:44:29 -0700

    Save Progress

---
+++ VK_SANDBOX/Application/src/SampleApplication/CustomBehaviors/AnimationTester.cpp
@@ -0,0 +1,107 @@
+#include <glm/gtx/string_cast.hpp> 
+#include "AnimationTester.h"
+
+#include "../../Engine/src/Core/Locator/AssetManagerLocator.h"
+#include "../../../../Engine/src/Core/Scene/GameObject.h"
+#include "../../Engine/src/Core/Locator/AppWindowLocator.h"
+#include "../../Engine/src/Core/Time/Time.h"
+#include "../../Engine/src/Core/Scene/Components/AnimatorComponent.h"
+
+namespace SampleApplication
+{
+	AnimationTester::AnimationTester()
+	{
+	}
+
+	void AnimationTester::Awake()
+	{
+		window = IHCEngine::Core::AppWindowLocator::GetAppWindow()->GetWindowHandle();
+		for(int i=0; i< animationGobjs.size(); ++i)
+		{
+			animationGobjs[i]->SetActive(false);
+		}
+		animationGobjs[0]->SetActive(true);
+		animationGobjs[0]->GetComponent<IHCEngine::Component::AnimatorComponent>()
+			->SetAnimation(animationSlots1[0]);
+	}
+
+	void AnimationTester::Start()
+	{
+	}
+
+	void AnimationTester::Update()
+	{
+		bool isRightKeyPressed = glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS;
+		if (isRightKeyPressed && !wasRightKeyPressed) 
+		{
+			animationGobjs[modelIndex]->SetActive(false);
+			modelIndex = (modelIndex + 1) % animationGobjs.size();
+			animationGobjs[modelIndex]->SetActive(true);
+
+			if (animationIndex == 0)
+			{
+				animationGobjs[modelIndex]->GetComponent<IHCEngine::Component::AnimatorComponent>()
+					->SetAnimation(animationSlots1[modelIndex]);
+			}
+			else
+			{
+				animationGobjs[modelIndex]->GetComponent<IHCEngine::Component::AnimatorComponent>()
+					->SetAnimation(animationSlots2[modelIndex]);
+			}
+		}
+		wasRightKeyPressed = isRightKeyPressed;
+
+		bool isLeftKeyPressed = glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS;
+		if (isLeftKeyPressed && !wasLeftKeyPressed)
+		{
+			animationGobjs[modelIndex]->SetActive(false);
+			modelIndex = (modelIndex - 1) % animationGobjs.size();
+			animationGobjs[modelIndex]->SetActive(true);
+
+			if (animationIndex == 0)
+			{
+				animationGobjs[modelIndex]->GetComponent<IHCEngine::Component::AnimatorComponent>()
+					->SetAnimation(animationSlots1[modelIndex]);
+			}
+			else
+			{
+				animationGobjs[modelIndex]->GetComponent<IHCEngine::Component::AnimatorComponent>()
+					->SetAnimation(animationSlots2[modelIndex]);
+			}
+		}
+		wasLeftKeyPressed = isLeftKeyPressed;
+
+		bool isSpaceKeyPressed = glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS;
+		if (isSpaceKeyPressed && !wasSpaceKeyPressed) 
+		{
+			animationIndex = (animationIndex + 1) % 2;
+			if (animationIndex == 0)
+			{
+				animationGobjs[modelIndex]->GetComponent<IHCEngine::Component::AnimatorComponent>()
+					->SetAnimation(animationSlots1[modelIndex]);
+			}
+			else
+			{
+				animationGobjs[modelIndex]->GetComponent<IHCEngine::Component::AnimatorComponent>()
+					->SetAnimation(animationSlots2[modelIndex]);
+			}
+		}
+		wasSpaceKeyPressed = isSpaceKeyPressed;
+
+	}
+
+	void AnimationTester::FixedUpdate()
+	{
+		
+	}
+
+	void AnimationTester::OnEnable()
+	{
+		
+	}
+
+	void AnimationTester::OnDisable()
+	{
+		
+	}
+}
---
+++ VK_SANDBOX/Application/src/SampleApplication/CustomBehaviors/AnimationTester.h
@@ -0,0 +1,53 @@
+
+#pragma once
+#include "../../../../Engine/src/Core/Scene/GameObject.h"
+#include "../../../../Engine/src/Graphics/GraphicsManager.h"
+#include "../../Engine/src/Core/Scene/Components/CustomBehavior/CustomBehavior.h"
+
+namespace IHCEngine::Graphics
+{
+	class Animation;
+}
+
+namespace SampleApplication
+{
+    class AnimationTester : public IHCEngine::Component::CustomBehavior
+    {
+    public:
+        AnimationTester();
+        void Awake() override;
+        void Start() override;
+        void Update() override;
+        void FixedUpdate() override;
+        void OnEnable() override;
+        void OnDisable() override;
+
+        void AddAnimationGobjs(IHCEngine::Core::GameObject* g)
+        {
+            animationGobjs.push_back(g);
+        }
+        void AddAnimationSlots1(IHCEngine::Graphics::Animation* a)
+        {
+            animationSlots1.push_back(a);
+        }
+        void AddAnimationSlots2(IHCEngine::Graphics::Animation* a)
+        {
+            animationSlots2.push_back(a);
+        }
+
+    private:
+        GLFWwindow* window = nullptr;
+
+        // Make each animation Models has corresponding 2 animations
+        std::vector<IHCEngine::Core::GameObject*> animationGobjs;
+        std::vector<IHCEngine::Graphics::Animation*> animationSlots1;
+        std::vector<IHCEngine::Graphics::Animation*> animationSlots2;
+        int modelIndex = 0;
+        int animationIndex = 0;
+
+
+        bool wasRightKeyPressed = false;
+        bool wasLeftKeyPressed = false;
+        bool wasSpaceKeyPressed = false;
+    };
+}
